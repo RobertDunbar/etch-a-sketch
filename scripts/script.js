fb84a8ea103918ff06836b28968373182c759f88
Robert Dunbar <robert_dunbar@me.com>
2019-05-27 17:04:16 +0100

    grid finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 // Odin Project etch-a-sketch javascript file
+const container = document.querySelector("#container");
+for(rowCount = 0; rowCount <16; rowCount++){
+    const brContent = document.createElement("br");
+    for(colCount = 0; colCount <16; colCount++){
+        const divContent = document.createElement("div");
+        divContent.classList.add("divGrid");
+        if(rowCount === 0 || rowCount === 15) divContent.classList.add("r" + rowCount);
+        if(colCount === 0 || colCount === 15) divContent.classList.add("c" + colCount);
+        container.appendChild(divContent);
+    }
+    container.appendChild(brContent);
+}
