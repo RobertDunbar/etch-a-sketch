25de8aa1d8a1c25a253b64cee57c19f1af4d7a92
Robert Dunbar <robert_dunbar@me.com>
2019-05-28 21:48:09 +0100

    finished project

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,76 @@
 // Odin Project etch-a-sketch javascript file
 const container = document.querySelector("#container");
-for(rowCount = 0; rowCount <16; rowCount++){
-    const brContent = document.createElement("br");
-    for(colCount = 0; colCount <16; colCount++){
-        const divContent = document.createElement("div");
-        divContent.classList.add("divGrid");
-        if(rowCount === 0 || rowCount === 15) divContent.classList.add("r" + rowCount);
-        if(colCount === 0 || colCount === 15) divContent.classList.add("c" + colCount);
-        container.appendChild(divContent);
+
+function generateGrid(rows, columns){
+    const squareSize = (560 - 8 - ((rows-1)*2)) / rows;
+    for(rowCount = 0; rowCount < rows; rowCount++){
+        const brContent = document.createElement("br");
+        for(colCount = 0; colCount < columns; colCount++){
+            const divContent = document.createElement("div");
+            divContent.classList.add("divGrid");
+            divContent.style.width = squareSize + "px";
+            divContent.style.height = squareSize + "px";
+            switch (rowCount) {
+                case 0:
+                    divContent.classList.add("border-left");
+                    break;
+                case rows - 1:
+                    divContent.classList.add("border-right");
+            }
+            switch (colCount) {
+                case 0:
+                    divContent.classList.add("border-top");
+                    break;
+                case columns - 1:
+                    divContent.classList.add("border-bottom");
+            }
+            container.appendChild(divContent);
+        }
+        container.appendChild(brContent);
     }
-    container.appendChild(brContent);
 }
+
+function trailClass(e){
+    var thisColor = ColorOption === "black"? "black": randomColor();
+    e.target.style.backgroundColor = thisColor;
+}
+
+function randomColor(currentColor) {
+    var randomRed = Math.floor(Math.random() * 255);
+    var randomGreen = Math.floor(Math.random() * 255);
+    var randomBlue = Math.floor(Math.random() * 255);
+    var randomColor = "rgb(" + randomRed + "," + randomGreen + "," + randomBlue + ")";
+    return randomColor;
+}
+
+function clearGrid(){
+    const square = Array.from(document.querySelectorAll(".divGrid"));
+    square.forEach(grid => {
+        grid.removeEventListener("mouseover", trailClass);
+        container.removeChild(grid);
+    });
+}
+
+function listenGrid(){
+    const sqaure = Array.from(document.querySelectorAll(".divGrid"));
+    sqaure.forEach(grid => grid.addEventListener('mouseover', trailClass));
+}
+
+var ColorOption = "black";
+generateGrid(16, 16);
+listenGrid();
+
+const radios = Array.from(document.querySelectorAll("input[type=radio]"));
+radios.forEach(radio => {
+    radio.addEventListener('change', () => {
+        ColorOption = radio.value;
+    });
+});
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    clearGrid();
+    var numSquares = prompt("Number of sqaures on an axis?", "16");
+    generateGrid(numSquares, numSquares);
+    listenGrid();
+});
